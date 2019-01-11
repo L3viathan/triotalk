1fab1ddb31eb009df9d4a7348ce8b77ab54a64bd
Jonathan Oberländer <jo@solute.de>
2019-01-11 15:08:17 +0100

    cheat sheet

---
+++ echo_v2.py
@@ -0,0 +1,15 @@
+import trio
+
+async def handler(conn):
+    ip, port, *_ = conn.socket.getpeername()
+    print(f"[{ip}]:{port} opened connection")
+    while True:
+        data = await conn.receive_some(1024)
+        print(f"[{ip}]:{port} sent data")
+        await conn.send_all(data)
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(trio.serve_tcp, handler, 1234)
+
+trio.run(main)
